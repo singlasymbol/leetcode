5d9c65e8cf879600936e7036c637aec0a0dcad42
Symbol Singla <singla.symbol@juspay.in>
2020-11-15 18:37:20 +0530

    Add solution approach, problem link and more comments in code

--- easy/DefuseBomb.java
+++ easy/DefuseBomb.java
@@ -1,5 +1,9 @@
+// Problem link
+// https://leetcode.com/problems/defuse-the-bomb/submissions/
+
 class Solution {
-    
+
+    // This function is used to select the relevant index when the range goes out of arrayLength.    
     public int getNextElement(int index, int length) {
         if (index >= length || index < 0) {
             return (index % length);
@@ -8,6 +12,10 @@ class Solution {
         return index;
     }
     
+    // SolutionApproach: Keep an initial window sum using length k, and then keep on subtracting the next element and adding the window's new element as we move forward.
+    // for negative k- the approach turns into keeping a gap between the window and current element effectively that is (size - 1 - k).
+    // gap is used for cases when k is negative. the idea is to use one logic and just add a gap.
+    // default value is zero for cases when k is positive
     public int[] decrypt(int[] code, int k) {
         int n = code.length;
         
@@ -19,7 +27,7 @@ class Solution {
             int currSum = 0;
             int gap = 0;
             if(k < 0) {
-                gap = n + k - 1;
+                gap = n + k - 1; 
             }
             k = Math.abs(k);
             int [] resultArray = new int[n];
