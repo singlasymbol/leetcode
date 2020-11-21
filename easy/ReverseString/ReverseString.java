e1e493e232de2118230d07e42e9deed784ba909c
Symbol Singla <singla.symbol@juspay.in>
2020-11-22 00:24:15 +0530

    Add solution to reverse string 344

---
+++ easy/ReverseString/ReverseString.java
@@ -0,0 +1,10 @@
+// PS : https://leetcode.com/problems/reverse-string/
+class Solution {
+    public void reverseString(char[] s) {
+        for(int i = 0; i < s.length/2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - i -1];
+            s[s.length - i -1] = temp;
+        }
+    }
+}
