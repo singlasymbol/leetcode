9c4b6ef4ee36869196e015b9b51f6954745b852b
Symbol Singla <singla.symbol@juspay.in>
2020-11-27 00:11:31 +0530

    Add solution to no of one bits 191

---
+++ easy/NoOfOneBits/NoOfOneBits.java
@@ -0,0 +1,15 @@
+// PS: https://leetcode.com/problems/number-of-1-bits/
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int sum = 0;
+        while(n != 0) {
+            if((n & 1) == 1) {
+                sum++;
+            }
+            n = n >>> 1;
+        }
+        
+        return sum;
+    }
+}
