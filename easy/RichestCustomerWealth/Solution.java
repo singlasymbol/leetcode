ff7bc67f011f25d37153823a473045db8cfb7bc6
Symbol Singla <singla.symbol@juspay.in>
2020-11-30 23:12:00 +0530

    Add solution to richest customer wealth 1672

---
+++ easy/RichestCustomerWealth/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        int sum = 0;
+        
+        for(int i = 0; i < accounts.length; i++) {
+            for(int j = 0; j < accounts[i].length; j++) {
+                sum = sum + accounts[i][j];    
+            }
+            
+            max = Math.max(max, sum);
+            sum = 0;
+        }
+        
+        return max;
+    }
+}
